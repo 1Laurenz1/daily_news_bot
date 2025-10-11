90065bece565128d188a8167d0bc3d73779aa242
Laurenz <ddanilenko350@gmail.com>
2025-10-11 10:31:06 +0300

    Add create_base

---
+++ app/database/models/__init__.py
@@ -0,0 +1,1 @@
+from .create_base import engine, Base, AsyncSessionLocal
---
+++ app/database/models/create_base.py
@@ -0,0 +1,25 @@
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import (
+    AsyncAttrs,
+    create_async_engine,
+    async_sessionmaker,
+    AsyncSession
+)
+
+from app.core import settings
+
+
+class Base(AsyncAttrs, DeclarativeBase):
+    pass
+
+
+engine = create_async_engine(url=settings.DATABASE_URL.get_secret_value())
+
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    expire_on_commit=True,
+    echo=True,
+    class_=AsyncSession,
+    autoflush=False,
+    autocommit=False
+)
